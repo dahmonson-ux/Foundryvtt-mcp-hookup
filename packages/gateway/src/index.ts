5247bfefa65f36478e4b8ce86e563c9ef870398a
dahmonson-ux <dahmonson@gmail.com>
2026-09-21 18:34:46 -0500

    Export canonical reference policy

--- packages/gateway/src/index.ts
+++ packages/gateway/src/index.ts
@@ -2,4 +2,5 @@ export * from "./ports.js";
 export * from "./authorization.js";
 export * from "./idempotency.js";
 export * from "./config.js";
+export * from "./reference-policy.js";
 export * from "./gateway.js";
