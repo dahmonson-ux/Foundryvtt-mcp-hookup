6bc5c2d3c879bfb0b44da7bb01f59a13ef49ee77
dahmonson-ux <dahmonson@gmail.com>
2026-09-21 18:34:49 -0500

    Export Assistant DM affordances

--- packages/affordances/src/index.ts
+++ packages/affordances/src/index.ts
@@ -2,3 +2,4 @@ export * from "./resolver.js";
 export * from "./combat-resolvers.js";
 export * from "./world-resolvers.js";
 export * from "./conditional-resolvers.js";
+export * from "./dm-resolvers.js";
